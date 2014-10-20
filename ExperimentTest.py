c985bfc0183b01744ede6e80a5a4bf982cb9a73c
annyan09023 <ann.yanxuebin@gmail.com>
2014-10-19 23:29:06 -0600

    adding more test cases

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -11,24 +11,24 @@ class MyTestCase(unittest.TestCase):
     r1 = Experiment.largest([3,2,1]) 
     self.assertEqual(r1, 3)
 
-#  def test_t3(self):
-#    r1 = Experiment.largest([3,2,3]) 
-#    self.assertEqual(r1, 3)
-#
-#  def test_t4(self):
-#    r1 = Experiment.largest([3,2,3,4]) 
-#    self.assertEqual(r1, 4)
-#
-#  def test_t5(self):
-#    r1 = Experiment.largest([3,-2,3,-1,4]) 
-#    self.assertEqual(r1, 4)
-#
-#  def test_t6(self):
-#    r1 = Experiment.largest([-3,-2,-3,-1,-4]) 
-#    self.assertEqual(r1, -1)
-#
-#  def test_t7(self):
-#    self.assertRaises(ValueError, Experiment.largest, [])
+  def test_t3(self):
+    r1 = Experiment.largest([3,2,3]) 
+    self.assertEqual(r1, 3)
+
+  def test_t4(self):
+    r1 = Experiment.largest([3,2,3,4]) 
+    self.assertEqual(r1, 4)
+
+  def test_t5(self):
+    r1 = Experiment.largest([3,-2,3,-1,4]) 
+    self.assertEqual(r1, 4)
+
+  def test_t6(self):
+    r1 = Experiment.largest([-3,-2,-3,-1,-4]) 
+    self.assertEqual(r1, -1)
+
+  def test_t7(self):
+    self.assertRaises(ValueError, Experiment.largest, [])
 
 if __name__ == '__main__':
   unittest.main()
